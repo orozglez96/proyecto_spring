91cf73c35e65dce13886b450be9ad6d0fdbfa504
victor <vm.orozglez@gmail.com>
2026-03-04 16:25:06 +0100

    todos los proyectos migrados a springboot

---
+++ 07_libreria_springboot/src/main/java/model/Libro.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Libro {
+	private String isbn;
+	private String titulo;
+	private String autor;
+	private double precio;
+	
+	
+}
